0b94ee092f79b304bbaba5f20839923e129bbb3b
Mark Longair <mhl@pobox.com>
2018-06-05 18:00:19 +0100

    Make "require 'commons/integrity'" work
    It was a bit awkward working out which code needed to be required, so
    this you do:
    
      require 'commons/integrity'
    
    ... to include all checks, the report and config.

---
+++ lib/commons/integrity.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'require_all'
+
+require_rel 'integrity/check'
+require_rel 'integrity/report'
+
+module Commons
+  # This module contains all the library code for checking the
+  # integrity of data.
+  module Integrity
+  end
+end
